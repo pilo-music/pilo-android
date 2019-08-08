aa9ff7abd8013b7063a448e1f593d81bac84132c
mobin <mobinyazdanparast(@gmail.com>
2019-08-09 01:26:46 +0430

    init

---
+++ app/src/main/java/app/pilo/android/activities/MainActivity.java
@@ -0,0 +1,115 @@
+package app.pilo.android.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+import app.pilo.android.R;
+import app.pilo.android.fragments.BrowserFragment;
+import app.pilo.android.fragments.HomeFragment;
+import app.pilo.android.fragments.ProfileFragment;
+import app.pilo.android.fragments.SearchFragment;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import me.ibrahimsn.lib.NiceBottomBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.bottomBar)
+    NiceBottomBar bottomBar;
+
+    private boolean doubleBackToExitPressedOnce = false;
+    private Unbinder unbinder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        unbinder = ButterKnife.bind(this);
+        setupBottomNavigation();
+    }
+
+    private void loadFragment(Fragment fragment, String tag) {
+        FragmentManager fm = getSupportFragmentManager();
+        for (int i = 0; i < fm.getBackStackEntryCount(); ++i) {
+            fm.popBackStack();
+        }
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        Fragment curFrag = getSupportFragmentManager().getPrimaryNavigationFragment();
+        if (curFrag != null) {
+            transaction.detach(curFrag);
+        }
+        Fragment fragment1 = getSupportFragmentManager().findFragmentByTag(tag);
+        if (fragment1 == null) {
+            fragment1 = fragment;
+            transaction.add(R.id.framelayout, fragment1, tag);
+        } else {
+            transaction.attach(fragment1);
+        }
+        transaction.setPrimaryNavigationFragment(fragment1);
+        transaction.setReorderingAllowed(true);
+        transaction.commitNowAllowingStateLoss();
+    }
+
+    private void setupBottomNavigation() {
+        bottomBar.setBottomBarCallback(new NiceBottomBar.BottomBarCallback() {
+            @Override
+            public void onItemSelect(int i) {
+                switch (i) {
+                    case 0:
+                        loadFragment(new HomeFragment(), "MAIN_HOME_FRAGMENT");
+                        break;
+                    case 1:
+                        loadFragment(new BrowserFragment(), "MAIN_BROWSER_FRAGMENT");
+                        break;
+                    case 2:
+                        loadFragment(new SearchFragment(), "MAIN_SEARCH_FRAGMENT");
+                        break;
+                    case 3:
+                        loadFragment(new ProfileFragment(), "MAIN_PROFILE_FRAGMENT");
+                        break;
+                }
+            }
+
+            @Override
+            public void onItemReselect(int i) {
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        try {
+            if (doubleBackToExitPressedOnce) {
+                super.onBackPressed();
+                finish();
+            } else {
+                this.doubleBackToExitPressedOnce = true;
+                Toast.makeText(this, R.string.exit_message, Toast.LENGTH_SHORT).show();
+                new Handler().postDelayed(() -> doubleBackToExitPressedOnce = false, 2000);
+            }
+        } catch (
+                Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        this.doubleBackToExitPressedOnce = false;
+    }
+
+    @Override
+    protected void onDestroy() {
+        unbinder.unbind();
+        super.onDestroy();
+    }
+}
