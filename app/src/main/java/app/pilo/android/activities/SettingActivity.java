3dc8598c599eaadae1f0ccf257d91b8192861f76
mobin <mobinyazdanparast9@gmail.com>
2020-04-19 19:47:02 +0430

    adding listener to all pages for updating adapters

---
+++ app/src/main/java/app/pilo/android/activities/SettingActivity.java
@@ -0,0 +1,15 @@
+package app.pilo.android.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import app.pilo.android.R;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
