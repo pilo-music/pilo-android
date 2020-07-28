b14fb9c2d58bd430fbfe2d5ded75b9bace0d90ef
mohamad <mamady83@gmail.com>
2020-07-28 20:30:48 +0430

    mamad

--- app/src/main/java/app/pilo/android/activities/BaseActivity.java
+++ app/src/main/java/app/pilo/android/activities/BaseActivity.java
@@ -8,9 +8,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity {
 
+    public boolean active = false;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        active = true;
     }
 
     @Override
@@ -20,6 +22,12 @@ public class BaseActivity extends AppCompatActivity {
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         getWindow().setStatusBarColor(Color.TRANSPARENT);
+        active = true;
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        active = false;
+    }
 }
--- app/src/main/java/app/pilo/android/activities/SplashScreenActivity.java
+++ app/src/main/java/app/pilo/android/activities/SplashScreenActivity.java
@@ -1,7 +1,6 @@
 package app.pilo.android.activities;
 
 import androidx.annotation.Nullable;
-
 import app.pilo.android.BuildConfig;
 import app.pilo.android.R;
 import app.pilo.android.api.HttpErrorHandler;
@@ -9,39 +8,23 @@ import app.pilo.android.api.HttpHandler;
 import app.pilo.android.api.VersionApi;
 import app.pilo.android.repositories.UserRepo;
 import app.pilo.android.views.UpdateDialog;
-import butterknife.BindView;
 import butterknife.ButterKnife;
-import fr.castorflex.android.circularprogressbar.CircularProgressBar;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
-import android.view.View;
-import android.widget.LinearLayout;
-
 import com.android.volley.error.VolleyError;
-
 import java.util.Map;
 
 public class SplashScreenActivity extends BaseActivity {
-    @BindView(R.id.cpb_splash)
-    CircularProgressBar cpb_splash;
-    @BindView(R.id.ll_try_again)
-    LinearLayout ll_try_again;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
-        ButterKnife.bind(this);
         checkUpdate();
-        ll_try_again.setOnClickListener(v -> checkUpdate());
     }
 
     private void checkUpdate() {
-        cpb_splash.setVisibility(View.VISIBLE);
-        ll_try_again.setVisibility(View.GONE);
         new VersionApi(this).get(new HttpHandler.RequestHandler() {
             @Override
             public void onGetInfo(Object data, String message, boolean status) {
@@ -61,16 +44,12 @@ public class SplashScreenActivity extends BaseActivity {
                     }
 
                 } else {
-                    cpb_splash.setVisibility(View.GONE);
-                    ll_try_again.setVisibility(View.VISIBLE);
                     new HttpErrorHandler(SplashScreenActivity.this, message);
                 }
             }
 
             @Override
             public void onGetError(@Nullable VolleyError error) {
-                cpb_splash.setVisibility(View.GONE);
-                ll_try_again.setVisibility(View.VISIBLE);
                 new HttpErrorHandler(SplashScreenActivity.this);
                 Log.e("error", "onGetError: " + error.getMessage());
             }
@@ -85,14 +64,11 @@ public class SplashScreenActivity extends BaseActivity {
     public void checkForLogin() {
         String token = UserRepo.getInstance(this).get().getAccess_token();
 
-        final Handler handler = new Handler();
-        handler.postDelayed(() -> {
-            if (token != null && !token.equals(""))
-                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
-            else
-                startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
-            finish();
-        }, 1000);
-
+        if (token != null && !token.equals("")) {
+            startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+        } else {
+            startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
+        }
+        finish();
     }
 }
